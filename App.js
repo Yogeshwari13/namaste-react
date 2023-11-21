0f8c840445c57f190e4daa842cb3e7f64b83183c
Yogeshwari13 <yogeshwari.t@xerox.com>
2023-11-22 01:09:26 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,7 @@
+const divv = React.createElement("div",{},[
+    React.createElement("h1",{},"A"),
+    React.createElement("h2",{},"B"),
+])
+
+const x = ReactDOM.createRoot(document.getElementById("root"));
+x.render(divv);
